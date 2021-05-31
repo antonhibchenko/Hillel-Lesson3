60fa811cdea2e4b53e5ac7ccc279d9ba55f0c91e
antonhibchenko <sl1pgcp@gmail.com>
2021-05-31 21:33:36 +0300

    module vpc

--- main.tf
+++ main.tf
@@ -15,3 +15,10 @@ module "vpc" {
   vpc_cidr = "10.0.0.0/16"
   tags     = local.common_tags
 }
+
+module "ec2" {
+  source = "./modules/ec2"
+  instance_type = var.instance_type
+  name = var.name
+  subnet_id = module.vpc.public_networks[1]
+}
